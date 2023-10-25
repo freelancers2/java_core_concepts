88b3ac3c9625caf989dd17a0f039861b5922fe82
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-25 23:38:19 +0200

    last commit

--- main/java/src/collections/Main.java
+++ main/java/src/collections/Main.java
@@ -2,4 +2,42 @@ package collections;
 
 public class Main {
 
-}
+  public static void main(String[] args) {
+    // 1D Arrays :
+    String[] fruits = {"banana", "oranges", "apples"};
+    int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
+
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("Array element  is " + numbers[i]);
+      System.out.println("Iteration number " + i);
+
+    }
+
+    for (String fruit : fruits){
+      System.out.println(fruit);
+    }
+
+
+    // 2D Arrays
+
+    int[][] digits = { {10,15,20 }, {50,75,100 },{85,65,45 } };
+
+    for (int i = 0; i < digits.length; i++) {
+      System.out.println("Row number "+i);
+      for (int j = 0; j < digits[i].length; j++) {
+        System.out.println(digits[i][j]);
+      }
+
+    }
+
+
+    for(int[] innerArray : digits){
+      for (int x : innerArray ){
+        System.out.println(x);
+      }
+    }
+
+    }
+
+  }
+
